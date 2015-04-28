a18f8d07d7ccf498f9fe98ba0604003e79f08dbb
wapmorgan <wapmorgan@gmail.com>
2015-04-28 21:01:09 +0400

    Update php 5.4 deprecations.

--- data/5.4.php
+++ data/5.4.php
@@ -1,7 +1,9 @@
 <?php
 return array(
     'functions' => array(
-        'mcrypt_generic_end',
+        'mcrypt_generic_end' => 'mcrypt_generic_deinit',
+        'set_magic_quotes_runtime',
+        'magic_quotes_runtime',
         'mysql_list_dbs',
     )
 );
