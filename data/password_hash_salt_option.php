5c5e7a7bb24ad0a8daaf3d1e149413b1c8a12355
wapmorgan <wapmorgan@gmail.com>
2017-03-03 16:19:58 +0300

    Fix for checking password_hash() function

--- data/password_hash_salt_option.php
+++ data/password_hash_salt_option.php
@@ -3,10 +3,13 @@ namespace wapmorgan\PhpCodeFixer;
 
 function password_hash_salt_option(array $usage_tokens) {
     $tree = PhpCodeFixer::makeFunctionCallTree($usage_tokens);
+    // if no extra options passed
+    if (!isset($tree[1]))
+        return false;
     $data = PhpCodeFixer::delimByComma($tree[1]);
     $data = PhpCodeFixer::trimSpaces($data);
 
-    // getting delimiter
+    // searching for 'salt' option
     foreach ($data as $array_element) {
         if ($array_element[0][0] == T_CONSTANT_ENCAPSED_STRING) {
             $element_key = trim($array_element[0][1], '\'"');
