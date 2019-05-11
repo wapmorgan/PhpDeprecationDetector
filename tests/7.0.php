5fa08ca3cb6622f5ac4c2aa748c842a67c991f0e
wapmorgan <wapmorgan@gmail.com>
2019-05-11 21:41:03 +0300

    add test for ini_get()

--- tests/7.0.php
+++ tests/7.0.php
@@ -7,6 +7,8 @@ class test {
 
 mssql_connect();
 
+ini_get('always_populate_raw_post_data');
+
 ini_set('always_populate_raw_post_data', true);
 
 echo password_hash('123', PASSWORD_BCRYPT, array('cost' => 123, 'salt' => 'SALT'));
